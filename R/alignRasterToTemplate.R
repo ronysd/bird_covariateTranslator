c18e0f7a40705f33fe28c4a0454538a48ef2f4b1
Sourav Das <57733525+ronysd@users.noreply.github.com>
2026-09-24 10:27:53 -0400

    Update alignRasterToTemplate.R

--- R/alignRasterToTemplate.R
+++ R/alignRasterToTemplate.R
@@ -1,1 +1,54 @@
-
+alignRasterToTemplate <- function(
+    x,
+    template,
+    method = c("near", "bilinear"),
+    maskToTemplate = TRUE
+) {
+  
+  method <- match.arg(method)
+  
+  if (!inherits(x, "SpatRaster")) {
+    stop("`x` must be a SpatRaster.")
+  }
+  
+  if (!inherits(template, "SpatRaster")) {
+    stop("`template` must be a SpatRaster.")
+  }
+  
+  template <- template[[1]]
+  
+  if (!terra::same.crs(x, template)) {
+    
+    x <- terra::project(
+      x,
+      template,
+      method = method
+    )
+    
+  } else if (!terra::compareGeom(
+    x,
+    template,
+    stopOnError = FALSE
+  )) {
+    
+    x <- terra::resample(
+      x,
+      template,
+      method = method
+    )
+  }
+  
+  if (!terra::compareGeom(
+    x,
+    template,
+    stopOnError = FALSE
+  )) {
+    stop("Failed to align raster to template.")
+  }
+  
+  if (maskToTemplate) {
+    x <- terra::mask(x, template)
+  }
+  
+  return(x)
+}
